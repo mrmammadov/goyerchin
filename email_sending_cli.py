74f64eefeea6ba56fc4974eff140f8f45c3fd129
Qulu Mammadli <mammadov.qulu@gmail.com>
2021-06-06 16:30:58 +0200

    Initial Prototype created

---
+++ email_sending_cli.py
@@ -0,0 +1,48 @@
+import argparse
+import smtplib
+import os
+import sys
+
+# Environment Variables
+email = os.environ.get('EMAIL')
+password = os.environ.get('PASSWORD')
+
+# # CLI Installation
+# my_parser = argparse.ArgumentParser(prog='email', description='List of all the files in the path')
+
+# my_parser.add_argument('Person', metavar='person', type=str, help='the path to list')
+# my_parser.add_argument('Email', metavar='email', type=str, help='the path to list')
+
+
+# args = my_parser.parse_args()
+
+# person = args.Person
+# email_to = args.Email
+
+# msg = f'''
+# To: {email_to}
+
+# Dear {person},
+
+# I am going to be graduating from FAU with a degree in Information Systems (Masters) and looking for roles related to Python. 
+# I have included my CV in the attachments.
+# Looking forward to hear from you!
+
+# Best Regards,
+# Gulu Mammadli
+# '''
+
+
+
+def send_email(email_from, email_to, mail):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        smtp.login(email, password)
+
+        for e in email_to:
+            smtp.sendmail(email_from, e, mail)
+
+    print('Email sent!')
---
+++ mail/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from email_send.settings import BASE_DIR
+from email_sending_cli import send_email
+
+def home(request):
+    if request.method == 'GET':
+        return render(request, 'mail/home.html')
+    else:
+        email_from = request.POST.get('email_from')
+        email_to = request.POST.get('email_to')
+        mail = request.POST.get('email_text')
+        print(email_to.split(','))
+        send_email(email_from, email_to.split(','), mail)
+        return render(request, 'mail/home.html')
